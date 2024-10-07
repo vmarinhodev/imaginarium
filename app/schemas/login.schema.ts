cc249d5ed87bed4e7f143ed5114219196f55d514
Vitor Marinho <vitor@uw-s.nl>
2024-10-07 14:02:21 +0200

    refactored forms with zod

--- app/schemas/login.schema.ts
+++ app/schemas/login.schema.ts
@@ -1,15 +1,16 @@
 import { z } from 'zod';
 
-export const schemaLogin  = z.object({
+export const schemaLogin = z.object({
     email: z.string().email(),
     password: z
         .string()
-        .min(8, { message: 'Password must be at least 8 characters' })
+        .min(6, { message: 'Password must be at least 6 characters' })
         .regex(
             /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])/,
             {
                 message:
                     'Password must contain at least one uppercase letter, one lowercase letter, one number and one special character',
             }
-        )
+        ),
+    username: z.string().min(3, { message: 'Name must be at least 3 characters' }),
 });
--- components/forms/signup/page.tsx
+++ components/forms/signup/page.tsx
@@ -1,15 +1,89 @@
-import Link from "next/link"
+import React from "react";
+import { schemaLogin } from "@/app/schemas/login.schema";
+import { useForm, FieldPath, Control } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export default function SignUpPage() {
+  const form =
+    useForm<z.infer<typeof schemaLogin>>({
+      resolver: zodResolver(schemaLogin),
+      defaultValues: {
+        email: "",
+        username: "",
+        password: "",
+      },
+    })
+
+  const onSubmit = (values: z.infer<typeof schemaLogin>) => {
+    console.log("values", values)
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between ">
-      <section className="w-4/5 max-w-5xl" >
-        <div className="mb-8 flex flex-col gap-2">
-          <h1 className="text-3xl font-semi-bold">Sign Up</h1>
-          <p className="text-sm text-neutral-500">
-            Already have an account? <Link href="#" className="underline underline-offset-4">Login</Link>
-          </p>
-        </div>
-      </section>
-    </main>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <SignupFormField
+          name="email"
+          label="Email"
+          placeholder="Enter your email"
+          description="We'll never share your email."
+          inputType="email"
+          formControl={form.control}
+        />
+        <SignupFormField
+          name="password"
+          label="Password"
+          placeholder="Password"
+          description="Password must be at least 6 characters"
+          inputType="password"
+          formControl={form.control}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+};
+
+interface SignUpFormFieldProps {
+  name: FieldPath<z.infer<typeof schemaLogin>>;
+  label: string;
+  placeholder: string;
+  description?: string;
+  inputType?: string;
+  formControl: Control<z.infer<typeof schemaLogin>, any>;
+}
+
+const SignupFormField: React.FC<SignUpFormFieldProps> = ({
+  name, label, placeholder, description, inputType, formControl
+}) => {
+  return (
+    <FormField
+      name={name}
+      control={formControl}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <Input
+              placeholder={placeholder}
+              type={inputType || "text"}
+              {...field} />
+          </FormControl>
+          {description && <FormDescription>{description}</FormDescription>}
+          <FormMessage />
+        </FormItem>
+      )}
+    />
   )
 }
